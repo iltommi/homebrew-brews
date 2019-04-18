181194d24570dc06a6c8c074ee5c715899795ad8
Tommaso Vinci <tommaso.vinci@polytechnique.edu>
2019-04-18 09:54:04 +0200

    remove sphinx dep

--- smilei.rb
+++ smilei.rb
@@ -5,7 +5,6 @@ class Smilei < Formula
 
   depends_on "python"
   depends_on "hdf5-parallel"
-  depends_on "sphinx"
   
   fails_with :clang
   env :std
@@ -31,16 +30,11 @@ class Smilei < Formula
     bin.install "smilei"
     bin.install "smilei_test"
 
-    system "make", "doc"
-    share.install "build/html"
     share.install "benchmarks"
     
     libexec.install "happi"
     (libexec/"src").install "src/Python"
-    xy = Language::Python.major_minor_version "python3"
-    site_packages = "lib/python#{xy}/site-packages"
-    pth_contents = "import site; site.addsitedir('#{libexec}')\n"
-    (prefix/site_packages/"smilei.pth").write pth_contents
+    (prefix/"lib/python#{Language::Python.major_minor_version ENV["PYTHONEXE"]}/site-packages/smilei.pth").write "import site; site.addsitedir('#{libexec}')\n"
 
   end
   
@@ -53,9 +47,6 @@ class Smilei < Formula
     
         To install the happi post-processing dependencies, type
         pip3 install numpy ipython h5py pint matplotlib scipy
-        
-        Documentation can be opened with
-        open /usr/local/opt/smilei/share/html/index.html
         
         To update Smilei just type
         brew upgrade --fetch-HEAD smilei
