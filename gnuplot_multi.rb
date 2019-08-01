9a65d42e4631ca4477ebecd56188d87993812c02
Tommaso Vinci <tommaso.vinci@polytechnique.edu>
2019-08-01 10:10:01 +0200

    add minimal gnuplot

---
+++ gnuplot_multi.rb
@@ -0,0 +1,46 @@
+class Gnuplot < Formula
+  desc "Command-driven, interactive function plotting"
+  homepage "http://www.gnuplot.info/"
+  url "https://downloads.sourceforge.net/project/gnuplot/gnuplot/5.2.6/gnuplot-5.2.6.tar.gz"
+  sha256 "35dd8f013139e31b3028fac280ee12d4b1346d9bb5c501586d1b5a04ae7a94ee"
+  revision 1
+
+  head do
+    url "https://git.code.sf.net/p/gnuplot/gnuplot-main.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "libcerf"
+  depends_on "lua"
+  depends_on "pango"
+  depends_on "wxmac"
+
+  def install
+    args = %W[
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+      --without-tutorial
+      --with-wx=#{Formula["wxmac"].opt_prefix}/bin/
+    ]
+    
+    system "./prepare" if build.head?
+    system "./configure", *args
+    ENV.deparallelize # or else emacs tries to edit the same file with two threads
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/gnuplot", "-e", <<~EOS
+      set terminal dumb;
+      set output "#{testpath}/graph.txt";
+      plot sin(x);
+    EOS
+    assert_predicate testpath/"graph.txt", :exist?
+  end
+end
