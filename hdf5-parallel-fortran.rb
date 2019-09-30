7a6355120f84c5f0cf4834511bd7538e87db9985
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2019-09-30 14:22:58 -0400

    solve name issue

--- hdf5-parallel-fortran.rb
+++ hdf5-parallel-fortran.rb
@@ -1,4 +1,4 @@
-class Hdf5Parallel < Formula
+class Hdf5ParallelFortran < Formula
   desc "File format designed to store large amounts of data"
   homepage "https://www.hdfgroup.org/HDF5"
   url "https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.5/src/hdf5-1.10.5.tar.bz2"
